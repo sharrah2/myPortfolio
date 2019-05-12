48837668a48a201cdc60ae63165e6b7359c7719e
lilpkstud <lilpkstud@hotmail.com>
2019-05-11 19:50:12 -0700

    Email Page
    Updating Email Page

---
+++ views/email.php
@@ -0,0 +1,67 @@
+<style>
+   body{
+      font-family: 'Anonymous Pro', monospace !important;
+   }
+   #content{
+      margin-top: 50px;
+   }
+   #emailList{
+      margin-top: 50px;
+   }
+   .col-md{
+      padding: 15px 0px;
+   }
+</style>
+<?php
+   include_once('header/header.php');
+
+?>
+   <div id="content" class="container">
+      <div class="row">
+         <div class="col-md">
+            <img class="img-fluid" src="/img/email/emailHeader.png" alt="">
+         </div>
+      </div>
+      <div id="emailList" class="row">
+         <div class="col-md">
+            <a href="/views/email_page.php?email=14.png" target="_blank">
+               <img class="img-fluid" src="/img/email/JanEmail.jpg" alt="">
+            </a>
+         </div>
+         <div class="col-md">
+            <a href="/views/email_page.php?email=117.png" target="_blank">
+               <img class="img-fluid" src="/img/email/Jan2Email.jpg" alt="">
+            </a>
+         </div>
+         <div class="col-md">
+            <a href="/views/email_page.php?email=124.png" target="_blank">
+               <img class="img-fluid" src="/img/email/Jan3Email.jpg" alt="">
+            </a>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-md">
+            <a href="/views/email_page.php?email=310.png" target="_blank">
+               <img class="img-fluid" src="/img/email/MarchEmail.jpg" alt="">
+            </a>
+         </div>
+         <div class="col-md">
+            <a href="/views/email_page.php?email=43.png" target="_blank">
+               <img class="img-fluid" src="/img/email/AprilEmail.jpg" alt="">
+            </a>
+         </div>
+         <div class="col-md">
+            <a href="/views/email_page.php?email=510.png" target="_blank">
+               <img class="img-fluid" src="/img/email/MayEmail.jpg" alt="">
+            </a>
+         </div>
+      </div>
+   </div>
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ views/email_page.php
@@ -0,0 +1,49 @@
+<style>
+   body{
+      font-family: 'Anonymous Pro', monospace !important;
+   }
+   #content{
+      margin-top: 100px;
+   }
+</style>
+<?php
+   include_once('header/header.php');
+
+   //var_dump($_GET);
+
+   $pathway = getEmail();
+
+
+   //var_dump($pathway);
+   //die();
+
+   /**
+    * 
+    * Create a function to grab the specific pathway from the img/email folder
+    * 
+    */
+   function getEmail(){
+      $email = $_GET['email'];
+      $allEmail = array_diff(scandir("../img/email"), array('..', '.', '.DS_Store'));
+      
+      $id = array_search($email, $allEmail);
+
+      return $allEmail[$id];
+   }
+?>
+
+    <div id="content" class="container">
+       <div class="row">
+           <div class="col">
+               <img class="img-fluid" src="/img/email/<?=$pathway?>" alt="">
+            </div>
+       </div>
+    </div>
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
