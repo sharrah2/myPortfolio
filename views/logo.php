9fc23c9df183587f100be1a90a2d73944a18e928
lilpkstud <lilpkstud@hotmail.com>
2019-08-16 00:38:17 -0700

    Updated Logo Page
    Fixed Logo Page to show page and images

--- views/logo.php
+++ views/logo.php
@@ -1,80 +1,54 @@
 <?php
-   
-    $pathway = pathway();
+   include_once('header/header.php');
 
-    /**
-     * 
-     * Create a function to grab all pathways from the img/logo folder
-     * 
-     */
-    function pathway(){
-       $logo = $_GET['work'];
-       return array_diff(scandir("../img/$logo"), array('..', '.', '.DS_Store'));
-    }
+   $pathway = getAll();
 
-
-
+   /**
+    * 
+    * Create a function to grab all flyer images from the img/flyer folder
+    * 
+    */
+   function getAll(){
+      $tv = $_GET['work'];
+      $allLogo= array_diff(scandir("../img/logo"), array('.', '..', '.DS_Store', 'stfc'));
+      return $allLogo;
+   }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sharon Harrah | Portfolio</title>
-     <!--Under Construction-->
-     <script type="text/javascript">
-        function load() 
-        {
-            window.location.href = "../comingsoon.php";
-        }
-    </script>
-    <style>
-        body {
-            font-family: 'Anonymous Pro', monospace !important;
-
-        }
-        .thumbnail:hover {
-            position:relative;
-            top:-25px;
-            left:-35px;
-            width:350px;
-            height:auto;
-            display:block;
-            z-index:999;
-        }
-    </style>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Anonymous+Pro" rel="stylesheet">
-</head>
-<body onload="load()">       
-    <div class="container">
+    <div class="container" id="content">
         <div class="row">
-            <div class="col">
-                <h1>Wells Ministry</h1>
-            </div>
-            <div class="col">
-                <h1>STFC</h1>
-            </div>
-            <div class="col">
-                <h1>ASUWT</h1>
+            <div class="col-md">
+                <img class="img-fluid" src="/img/objective/TVHeader.png" alt="">
             </div>
         </div>
         <div class="row">
-            <?php foreach($pathway as $path){ ?>
-                <div class="col-3">
-                    <img src="../img/logo/<?=$path?>" height="200" width="200" alt="" class="thumbnail" style="background-color: red; margin: 0px 10px;">
+            <?php 
+            foreach($pathway as $path) { ?>
+                <div class="col-sm-6 col-md-4 work">
+                    <img src="../img/logo/<?=$path?>" alt="../img/logo/<?=$path?>" class="workImg img-fluid">
                 </div>
-            <?php }?>
+            <?php } ?>
+        </div>
+        <div class="row">
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+                <!-- The Close Button -->
+                <span class="close">&times;</span>
+
+                <!-- Modal Content (The Image) -->
+                <img class="modal-content" id="img01">
+
+                <!-- Modal Caption (Image Text) -->
+                <div id="caption">
+                </div>
+            </div>
         </div>
     </div>
+    <script src="/js/modal.js"></script>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
 </body>
 </html>
-
